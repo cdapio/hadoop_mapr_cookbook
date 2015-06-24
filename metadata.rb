61ee9bdffad6e8176ca1b046db371a7fe1fb6aa2
dereklwood <derek@continuuity.com>
2015-06-24 13:29:08 -0700

    cleanup

--- metadata.rb
+++ metadata.rb
@@ -12,16 +12,8 @@ depends 'yum', '>= 3.0'
   depends cb
 end
 
-# %w(apt selinux sysctl ulimit).each do |cb|
-#   depends cb
-# end
-
 recommends 'java', '~> 1.21'
 
 %w(amazon centos debian redhat scientific ubuntu).each do |os|
   supports os
 end
-
-# %w(hadoop_cluster hadoop_cluster_rpm hadoop_for_hbase hbase hbase_cluster hive pig spark tez zookeeper zookeeper_cluster).each do |cb|
-#   conflicts cb
-# end
