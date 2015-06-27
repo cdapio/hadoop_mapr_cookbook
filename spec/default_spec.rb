fd5563dc8d5cf4dcf0c32ca84f66b2e4b56e5fcc
dereklwood <derek@continuuity.com>
2015-06-26 18:55:32 -0700

    test for alternate install dir

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -4,7 +4,7 @@ describe 'hadoop_mapr::default' do
   context 'on Centos 6.6' do
     let(:chef_run) do
       ChefSpec::SoloRunner.new(platform: 'centos', version: 6.6) do |node|
-        node.automatic['domain'] = 'example.com'
+        node.override['hadoop_mapr']['install_dir'] = '/some/data/disk'
       end.converge(described_recipe)
     end
 
@@ -15,6 +15,17 @@ describe 'hadoop_mapr::default' do
     it 'creates mapr user' do
       expect(chef_run).to create_user('mapr')
     end
+
+    it 'creates install dir' do
+      expect(chef_run).to create_directory('/some/data/disk')
+    end
+
+    it 'creates symlink' do
+      link = chef_run.link('/opt/mapr')
+      expect(link).to link_to('/some/data/disk')
+    end
+
+
   end
 
   context 'on Ubuntu 12.04' do
