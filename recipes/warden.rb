eb37190e6fda4b1d9bccabbc862c11ca817fa15c
dereklwood <derek@continuuity.com>
2015-07-01 14:46:19 -0700

    moving warden service to its own recipe

--- recipes/warden.rb
+++ recipes/warden.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: hadoop_mapr
-# Recipe:: fileserver
+# Recipe:: warden
 #
 # Copyright © 2013-2015 Cask Data, Inc.
 #
@@ -19,8 +19,12 @@
 
 include_recipe 'hadoop_mapr::default'
 
-pkg = 'mapr-fileserver'
+# The warden service is part of mapr-core-internal, which is a dependency of many mapr services.
+# Setting fileserver as the dependency here since it should be installed on every cluster node
+include_recipe 'hadoop_mapr::fileserver'
 
-package pkg do
-  action :install
+service 'mapr-warden' do
+  status_command "service mapr-warden status"
+  supports [:restart => true, :reload => true, :status => true]
+  action :nothing
 end
