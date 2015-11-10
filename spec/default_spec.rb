f303cffe76b1c6db9c26cf761141040f38e2d0a0
Derek Wood <derek@cask.co>
2015-11-10 14:45:41 -0800

    add spec for default install dir

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -36,9 +36,11 @@ describe 'hadoop_mapr::default' do
 
   context 'on Ubuntu 12.04' do
     let(:chef_run) do
-      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: 12.04) do |node|
-        node.automatic['domain'] = 'example.com'
-      end.converge(described_recipe)
+      ChefSpec::SoloRunner.new(platform: 'ubuntu', version: 12.04).converge(described_recipe)
+    end
+
+    it 'creates install dir' do
+      expect(chef_run).to create_directory('/opt/mapr')
     end
   end
 end
