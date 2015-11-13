ee91f24c68b459b4736df8bd30e1bc91fac75206
Derek Wood <derek@cask.co>
2015-11-13 12:26:11 -0800

    add chefspec matcher for configure lwrp

---
+++ libraries/matchers.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: hadoop_mapr
+# Library:: matchers
+#
+# Copyright © 2015 Cask Data, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+if defined?(ChefSpec)
+  ChefSpec.define_matcher :hadoop_mapr_configure
+  def run_hadoop_mapr_configure(cluster_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:hadoop_mapr_configure, :run, cluster_name)
+  end
+end
